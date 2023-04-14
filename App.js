753e6d917ebea6dd25932ddc05b99b9c6e6457b9
eduardovigil <54220799+eduardovigil@users.noreply.github.com>
2023-04-13 23:36:08 -0600

    Desafio 2 DPS Utilizacion de FETCH GET

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import  {React,useState,useEffect} from 'react';
+import { Text, View, StyleSheet, Image, Dimensions } from 'react-native';
+import Constants from 'expo-constants';
 
+const {width, height} = Dimensions.get('screen');
 export default function App() {
-  return (
+ const [data,setdata]= useState([]);
+  var url = 'https://api.tvmaze.com/shows';
+  fetch(url)
+    .then((response) => response.json())
+    .then((resjson) => setdata(resjson));
+  
+  return( 
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Text style={styles.titulo}>Series de television</Text>
+    {
+    data.map((show,i)=>(
+      <>
+      <Text key={i} style={styles.parrafo}>{show.name}</Text>
+      <Text style={styles.texto}>{show.genres}</Text>
+      <Image source={{uri: show.image.original}} style={styles.ima}/>
+      </>
+    ))}
     </View>
   );
 }
@@ -13,8 +28,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  ima:{width:width/8,
+      height:height/3,
+      borderRadius:'25px',
+      marginLeft:'60px',
+      marginBottom:'10px',
+      },
+  titulo:{ 
+    fontSize:'30px',
+    textAlign:'center',
+    marginTop:'20px',
+    marginBottom:'50px',
+    fontWeight:'bold',
+  },
+  parrafo:{
+    fontSize:'20px',
+    textAlign:'center',
+    marginBottom:'10px',
+  },
+  texto:{
+    fontSize:'15px',
+    textAlign:'center',
+    marginBottom:'10px',
   },
 });
